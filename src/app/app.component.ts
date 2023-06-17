e726f5f4a28e78e239cfa412301ebd26beaee5e2
joel <joel.isai@evosoft.es>
2023-06-17 15:14:16 +0200

    add 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cert';
+  
+  constructor() {
+
+  }
+
+
 }
